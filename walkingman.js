89f7f50ee6c82183d1687f5bbc9414891b41b079
bevieboo <bevin.sng@gmail.com>
2016-04-25 17:52:34 +1000

    man can move left and right on screen

--- walkingman.js
+++ walkingman.js
@@ -1,1 +1,19 @@
 console.log('hello');
+
+var count = 0;
+
+$(function(){
+  $('html').keydown(function(event){
+    var windowWidth = $(window).width();
+    var charWidth = $('.man').width();
+    var key = event.keyCode;
+    if (key === 39 && count <= (windowWidth - charWidth - 20)) {
+      count += 10;
+      var left = count+'px';
+    } else if (key === 37 && count >= 20) {
+      count -= 10;
+      var left = count+'px';
+    }
+    $('.man').css({ left: left });
+  })
+});
